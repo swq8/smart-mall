b5168e624789e57545b359d97b82017b42bf8fc1
alex <alex@mail.com>
2020-11-21 21:07:04 +0800

    edit README

--- src/main/java/alex/controller/admin/sys/Shipping.java
+++ src/main/java/alex/controller/admin/sys/Shipping.java
@@ -1,10 +1,28 @@
 package alex.controller.admin.sys;
 
+import alex.lib.Helper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller(value = "admin/sys/info")
+import javax.servlet.http.HttpServletRequest;
+
+@Controller(value = "admin/sys/shipping")
 @RequestMapping(path = "/admin/sys/shipping")
 public class Shipping {
 
+    @GetMapping(value = "")
+    public ModelAndView getIndex(HttpServletRequest request) {
+        ModelAndView modelAndView = Helper.newModelAndView("admin/other/shipping/index", request);
+        return modelAndView;
+    }
+
+    @PostMapping(value = "", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String postIndex(HttpServletRequest request) {
+        return "";
+    }
 }
